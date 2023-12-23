af7e34fe7613b37f20ca406323fae0975f6300ae
mirnatul <www.mirnatul@gmail.com>
2023-12-24 05:43:50 +0600

    basic route completed, add several pages and css libraries

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Main from './Layout/Main.jsx';
+import Home from './pages/Home/Home.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    children: [
+      {
+        path: '/',
+        element: <Home></Home>
+      }
+    ]
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <div className='max-w-[1420px] mx-auto'>
+      <RouterProvider router={router} />
+    </div>
   </React.StrictMode>,
 )
---
+++ client/src/pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            this is home
+            <button className='btn btn-primary'>Test button</button>
+        </div>
+    );
+};
+
+export default Home;
